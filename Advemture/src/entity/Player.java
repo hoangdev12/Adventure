40b9de7d979f1e8bc77e4dee5832876fe199378b
hoang12 <hoangluucoder1702@gmail.comgit config --global user.email hoangluucoder1702@gmail.com>
2024-04-01 21:45:46 +0700

    collisionupdate

--- Advemture/src/entity/Player.java
+++ Advemture/src/entity/Player.java
@@ -2,6 +2,7 @@ package entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -15,19 +16,34 @@ public class Player extends Entity{
 	GamePanel gp;
 	KeyHandler keyH;
 	
+	public final int screenX;
+	public final int screenY;
+	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		
 		this.gp = gp;
 		this.keyH = keyH;
 		
+		screenX = gp.screenWidth/2 - (gp.tileSize/2);
+		screenY = gp.screenHeight/2 - (gp.tileSize/2);
+		
+		//32x32
+		solidArea = new Rectangle();
+		solidArea.x = 8; 
+		solidArea.y = 16;
+		solidArea.width = 32;
+		solidArea.height = 32;
+		
 		setDefaultValues();
 		getPlayerImage();
+		
+		
 	}
 	
 	public void setDefaultValues() {
 		
-		x = 100;
-		y = 100;
+		worldX = gp.tileSize * 23;
+		worldY = gp.tileSize * 21;
 		speed = 4;
 		direction = "down";
 		
@@ -49,33 +65,58 @@ public class Player extends Entity{
 		}
 	}
 	public void update() {
-		if(keyH.upPressed == true) {
-			direction = "up";
-			y = y - speed;
-		}
-		else if(keyH.downPressed == true) {
-			direction = "down";
-			y += speed;
-		}
-		else if(keyH.leftPressed == true) {
-			direction = "left";
-			x -= speed;
-		}
-		else if(keyH.rightPressed == true) {
-			direction = "right";
-			x += speed;
-		}
-		
-		spriteCounter++;
-		if(spriteCounter > 8) {
-			if(spriteNum == 1) {
-				spriteNum = 2;
-			} else if(spriteNum ==2) {
-				spriteNum = 1;
-			}
-			spriteCounter = 0;
-		}
+	    
+	    if (keyH.upPressed == true) {
+	        direction = "up";
+	        movePlayer();
+	    } else if (keyH.downPressed == true) {
+	        direction = "down";
+	        movePlayer();
+	    } else if (keyH.leftPressed == true) {
+	        direction = "left";
+	        movePlayer();
+	    } else if (keyH.rightPressed == true) {
+	        direction = "right";
+	        movePlayer();
+	    }
+	    
+	    // update sprite
+	    spriteCounter++;
+	    if (spriteCounter > 12) {
+	        if (spriteNum == 1) {
+	            spriteNum = 2;
+	        } else if (spriteNum == 2) {
+	            spriteNum = 1;
+	        }
+	        
+	        spriteCounter = 0;
+	    }
 	}
+	
+	private void movePlayer() {
+	    
+	    collisionOn = false;
+	    gp.cChecker.checkTile(this);
+	    
+	   
+	    if (collisionOn == false) {
+	        switch (direction) {
+	            case "up":
+	                worldY -= speed;
+	                break;
+	            case "down":
+	                worldY += speed;
+	                break;
+	            case "left":
+	                worldX -= speed;
+	                break;
+	            case "right":
+	                worldX += speed;
+	                break;
+	        }
+	    }
+	}
+
 	public void draw(Graphics2D g2) {
         
 
@@ -118,6 +159,6 @@ public class Player extends Entity{
 			
 			break;
 		}
-		g2.drawImage(image,  x,  y,  gp.tileSize, gp.tileSize, null);
+		g2.drawImage(image,  screenX,  screenY,  gp.tileSize, gp.tileSize, null);
 	}
 }
--- Advemture/src/main/GamePanel.java
+++ Advemture/src/main/GamePanel.java
@@ -23,18 +23,26 @@ public class GamePanel extends JPanel implements Runnable{
 	public final int screenWidth = tileSize * maxScreenCol; //768 px
 	public final int screenHeight = tileSize * maxScreenRow; //576 px
 	
+	//WORLD SETTING	
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldHeight = tileSize * maxWorldRow;
+    public final int worldWidth = tileSize * maxWorldCol;
+	
+	
 	//FPS
 	int FPS = 60;
 	
 	TileManager tileM = new TileManager(this);
 	KeyHandler keyH = new KeyHandler();
 	Thread gameThread;
-	Player player = new Player(this,keyH);
+	public CollisionChecker cChecker = new CollisionChecker(this);
+	public Player player = new Player(this,keyH);
 	
-	//set player pos
-	int playerX = 100;
-	int playerY = 100;
-	int playerSpeed = 4;
+//	//set player pos
+//	int playerX = 100;
+//	int playerY = 100;
+//	int playerSpeed = 4;
 	
 	public GamePanel() {
 		
